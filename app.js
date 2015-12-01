967bf9e680d153f22a74642d359132171e6c5bdc
dougwells <dougwells@gmail.com>
2015-12-01 13:09:19 -0700

    L35:  Object.create & Inheritance

--- app.js
+++ app.js
@@ -1,36 +1,18 @@
-//merely changed require to the node file 'events.js'.  Clever!
-var Emitter = require('events');
-var eventConfig = require('./config.js').events
+var person = {
+	firstname: "",
+	lastname: "",
+	greet: function(){
+		return console.log(this.firstname + " " + this.lastname);
+	}
+};
 
-var emtr = new Emitter;
+var john = Object.create(person);
+john.firstname = "John";
+john.lastname = "Doe";
 
-emtr.on(eventConfig.GREET, function(){
-	console.log("Greet Triggered: Do fn1");
-});
+var jane = Object.create(person);
+jane.firstname = "Jane";
+jane.lastname = "Doe";
 
-emtr.on(eventConfig.GREET, function(){
-	console.log("Greet Triggered:  Do fn2");
-});
-
-emtr.on(eventConfig.FILESAVED, function(){
-	console.log("A file has just been saved");
-});
-
-emtr.on(eventConfig.FILESAVED, function(){
-	console.log("Yo Doug ... someone just saved a file");
-})
-
-var hardWork = function(){
-	console.log("Man ... saving files is hard work")
-}
-emtr.on(eventConfig.FILESAVED, hardWork);
-
-
-//changed code from emtr.events.greet --> emtr._events.greet
-console.log("hello");
-emtr.emit('greet');
-console.log(emtr._events.greet);
-
-console.log("Warning ... file save is about to happen");
-emtr.emit('fileSaved');
-console.log(emtr._events);
+jane.greet();
+john.greet();
