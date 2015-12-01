0ecede411513c853ca08bc2dc475997d585af4d4
dougwells <dougwells@gmail.com>
2015-11-30 22:01:54 -0700

    Node Emitters - Eliminated the Magic Strings - See config file + app.js file

--- app.js
+++ app.js
@@ -1,28 +1,29 @@
 //merely changed require to the node file 'events.js'.  Clever!
 var Emitter = require('events');
+var eventConfig = require('./config.js').events
 
 var emtr = new Emitter;
 
-emtr.on('greet', function(){
+emtr.on(eventConfig.GREET, function(){
 	console.log("Greet Triggered: Do fn1");
 });
 
-emtr.on('greet', function(){
+emtr.on(eventConfig.GREET, function(){
 	console.log("Greet Triggered:  Do fn2");
 });
 
-emtr.on('fileSaved', function(){
+emtr.on(eventConfig.FILESAVED, function(){
 	console.log("A file has just been saved");
 });
 
-emtr.on('fileSaved', function(){
+emtr.on(eventConfig.FILESAVED, function(){
 	console.log("Yo Doug ... someone just saved a file");
 })
 
 var hardWork = function(){
 	console.log("Man ... saving files is hard work")
 }
-emtr.on('fileSaved', hardWork);
+emtr.on(eventConfig.FILESAVED, hardWork);
 
 
 //changed code from emtr.events.greet --> emtr._events.greet
