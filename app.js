80c873616c0d2c943f5e9de0b4c14039741f0fdd
dougwells <dougwells@gmail.com>
2015-11-30 21:04:15 -0700

    Homemade Event Emitters ... Review

--- app.js
+++ app.js
@@ -10,5 +10,23 @@ emtr.on('greet', function(){
 	console.log("Greet Triggered:  Do fn2");
 });
 
+emtr.on('fileSaved', function(){
+	console.log("A file has just been saved");
+});
+
+emtr.on('fileSaved', function(){
+	console.log("Yo Doug ... someone just saved a file");
+})
+
+var hardWork = function(){
+	console.log("Man ... saving files is hard work")
+}
+emtr.on('fileSaved', hardWork);
+
 console.log("hello");
 emtr.emit('greet');
+console.log(emtr.events.greet);
+
+console.log("Warning ... file save is about to happen");
+emtr.emit('fileSaved');
+console.log(emtr.events);
