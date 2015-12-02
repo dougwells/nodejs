52c6dbfd7bb731af991fe3ea563491c8fa0247fc
dougwells <dougwells@gmail.com>
2015-12-01 19:37:43 -0700

    Invoking functions with .call & .apply

--- app.js
+++ app.js
@@ -1,28 +1,12 @@
-var EventEmitter = require('events');
-var util = require('util');
+var obj = {
+	name: "John Doe",
+	greet: function(param){console.log(`${param} ${this.name}`);}
+}  
 
-function Greetr (){
+var obj2 = {
+	name: "Doug Wells"
 }
 
-util.inherits(Greetr, EventEmitter);
-
-Greetr.prototype.greeting = "Hello World";
-
-Greetr.prototype.greet = function(param){
-	console.log(this.greeting + " from:"+ param);
-	this.emit('greet', param);
-}
-
-var greeter1 = new Greetr;
-greeter1.on('greet', function(param){
-	console.log("Someone Named "+param + " Greeted")
-});
-
-var greeter2 = new Greetr;
-greeter2.on('greet', function(param){
-	console.log("Someone Named "+param + " Greeted")
-});
-
-greeter1.greet("Doug");
-greeter2.greet("Linda")
-console.log(Greetr.prototype.greeting);
+obj.greet("Hello");
+obj.greet.call(obj2, "Hi");
+obj.greet.apply(obj,["Howdy"])
