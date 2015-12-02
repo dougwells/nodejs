119e0772290c181bd7e3facbab20d1e2f8a49155
dougwells <dougwells@gmail.com>
2015-12-01 22:41:01 -0700

    Using ES6 Classes as Modules.  Easy

--- app.js
+++ app.js
@@ -1,19 +1,6 @@
 'use strict';
 
-var EventEmitter = require('events');
-
-class Greetr extends EventEmitter{
-	constructor(){
-		super();
-		this.greeting = 'Hello world!';
-	}
-	greet (data){
-		console.log(`${this.greeting} My friend is ${data}`);
-		this.emit('greet', data);
-	}
-	
-}
-
+var Greetr = require('./greetr');
 var greeter1 = new Greetr();
 
 greeter1.on('greet', function(data) {
