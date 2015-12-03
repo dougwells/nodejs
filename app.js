3de1351b7e0de9c647a9d126adb19374a508faf0
dougwells <dougwells@gmail.com>
2015-12-03 13:16:30 -0700

    First Node Webserver using require http

--- app.js
+++ app.js
@@ -1,17 +1,14 @@
-var fs = require('fs');
+// grabs http object from Node
+var http = require('http');
 
-//zlib gives compression functions (in Node)
-var zlib = require('zlib');
+//sends name/value pair for data type
+//ends with line break which is good practice
+http.createServer(function(req, res){
+	res.writeHead(200, {"Content-Type": "text/plain"});
+	res.end("Hello World!! Dougs first WebServer Code!!\n");
+	
+// createServer has .listen function
+//1337 is like 3000.  127.0.0.1 is standard internal ip for local system (same as localhost)
+// localhost:1337.  Node server is listening at this port
 
-var readable = fs.createReadStream(__dirname + '/greet.txt', { encoding: 'utf8', highWaterMark: 16 * 1024 });
-var writable = fs.createWriteStream(__dirname + '/greetcopy.txt');
-var compressed = fs.createWriteStream(__dirname + '/greet.txt.gz');
-
-//createGzip creates a Transform Stream (can read and write)
-var gzip = zlib.createGzip();
-
-//can't pipe off of writeable since the output needs to be readable (write only stream)
-readable.pipe(writable);
-
-//readable (r only)--> pipe -->gzip(r/w) --> pipe --> compressed file (w only stream)
-readable.pipe(gzip).pipe(compressed);
+}).listen(1337, '127.0.0.1');
