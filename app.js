972b018ac35b03dbf279769939369f0159f94f0e
dougwells <dougwells@gmail.com>
2015-12-03 17:49:31 -0700

    Xfering HTML file via streams and asynch code

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
-// grabs http object from Node
+// grabs http object from Node & fs for handling files (html)
 var http = require('http');
+var fs = require('fs');
 
 //sends name/value pair for data type
 //ends with line break which is good practice
 http.createServer(function(req, res){
-	res.writeHead(200, {"Content-Type": "text/plain"});
-	res.end("Hello World!! Dougs first WebServer Code!!\n");
+	res.writeHead(200, {"Content-Type": "text/html"});
+	
+//make this code asynch.  Since a read stream, can pipe to a writestream, res
+	fs.createReadStream(__dirname + '/index.htm', 'utf8').pipe(res);
 	
 // createServer has .listen function
 //1337 is like 3000.  127.0.0.1 is standard internal ip for local system (same as localhost)
