2ed582844e50d96b5d7cf914e300ca7ddf3d10dc
dougwells <dougwells@gmail.com>
2015-12-03 23:19:23 -0700

    Node Routing.  Manually with if else statements

--- app.js
+++ app.js
@@ -1,15 +1,27 @@
-// grabs http object from Node & fs for handling files (html)
 var http = require('http');
 var fs = require('fs');
 
-//sends name/value pair for data type
-//ends with line break which is good practice
 http.createServer(function(req, res){
-	res.writeHead(200, {"Content-Type": "application/json"});
-	var testData = {
-		firstname: "John",
-		lastname: "Doe"
-	};
-	res.end(JSON.stringify(testData));
+	
+	//Routing ... cumbersome to do manually but it can be done
+	if(req.url ==='/'){
+		var htm = fs.createReadStream('./index.htm');
+		htm.pipe(res);
+	}
+	
+	else if(req.url ==="/api"){
+		res.writeHead(200, {"Content-Type": "application/json"});
+		var testData = {
+			firstname: "John",
+			lastname: "Doe"
+		};
+		res.end(JSON.stringify(testData));
+		
+//only run below error message if not an existing URL
+	}else{
+		res.writeHead(404);
+		res.end("404: Didi didi didi - Thats all folks!");
+	}
 
+//server is listening on localhost: 1337
 }).listen(1337, '127.0.0.1');
