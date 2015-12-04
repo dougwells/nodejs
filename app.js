52c3e843b2a560b1fde9d4cf47bd30feb7ee0b2d
dougwells <dougwells@gmail.com>
2015-12-03 20:11:41 -0700

    Building a simple API

--- app.js
+++ app.js
@@ -5,13 +5,11 @@ var fs = require('fs');
 //sends name/value pair for data type
 //ends with line break which is good practice
 http.createServer(function(req, res){
-	res.writeHead(200, {"Content-Type": "text/html"});
-	
-//make this code asynch.  Since a read stream, can pipe to a writestream, res
-	fs.createReadStream(__dirname + '/index.htm', 'utf8').pipe(res);
-	
-// createServer has .listen function
-//1337 is like 3000.  127.0.0.1 is standard internal ip for local system (same as localhost)
-// localhost:1337.  Node server is listening at this port
+	res.writeHead(200, {"Content-Type": "application/json"});
+	var testData = {
+		firstname: "John",
+		lastname: "Doe"
+	};
+	res.end(JSON.stringify(testData));
 
 }).listen(1337, '127.0.0.1');
