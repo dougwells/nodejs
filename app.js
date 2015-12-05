47fba0d86f90275fb46f58ded2fcc07714a117f7
dougwells <dougwells@gmail.com>
2015-12-05 15:11:26 -0700

    Grabbing values from POST Params.  Installed body-parse which is middleware for parsing POST params.

--- app.js
+++ app.js
@@ -8,11 +8,15 @@ app.listen = function listen() {
   return server.listen.apply(server, arguments);};
 */
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
+
+// create application/x-www-form-urlencoded parser
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
 
 //Environment Variable.  Can be set by server configuration.
 // || 3000 is "trick" to set default value if none.
-
 var port = process.env.PORT || 3000;
 app.listen(port);
 
@@ -37,10 +41,16 @@ app.get('/', function(req, res){
   res.render('index');
 });
 
-// ":" -> can be anything.  Variable is in the request (req) and
-// can access via params object.  Can have multiple vars (page & id)
+// ":" -> can be anything. Var on req.params object.  Mult. vars ok
+//Query String Variables -> the ?varName="Doug" or the ?qvar=123 in URL Bar
 app.get('/person/:id', function(req, res){
-  res.render('person', {ID: req.params.id, Qstr: req.query.qvar});
+  res.render('person', {ID: req.params.id, Qstr: req.query.qvar, Qname:req.query.qname});
+});
+
+app.post('/person', urlencodedParser, function(req, res){
+  if (!req.body) return res.sendStatus(400)
+  res.send('Welcome, ' + req.body.firstname+ "!")
+  console.log(req.body.lastname +" Logged In.")
 });
 
 app.get('/api', function(req, res){
